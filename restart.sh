f78d5bdcc706d64940af6d38d554abca36940972
Jeevan Pussella <scjeevan@gmail.com>
2016-06-30 00:38:20 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- restart.sh
+++ restart.sh
@@ -4,6 +4,6 @@ echo "Starting DataExport Server"
 forever -a -l start data-export-server/server.js &
 echo "Starting DataExport Client"
 cd data-export-client/
-forever -a -l client.log start node_modules/http-server/bin/http-server -a 0.0.0.0 -p 4141
+forever -a -l client.log start node_modules/http-server/bin/http-server -a 0.0.0.0 -p $DATAEXPORT_CLIENT_PORT
 
 cd ..
