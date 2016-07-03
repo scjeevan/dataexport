63d24f56f2aef0129478c0db644c9cc34a0654b4
Jeevan Pussella <scjeevan@gmail.com>
2016-07-03 20:19:58 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -12,7 +12,7 @@ var exportDataMng = {
 	exportData: function (req, res) {
 		var query = "";
 		var params = [];
-		var ftp_account = {};
+		var ftp_account = [];
 		var ftp_account_id = 1;
 		if (typeof req.body.ftp_account_id != 'undefined'){
 			ftp_account_id = parseInt(req.body.ftp_account_id);
@@ -26,10 +26,11 @@ var exportDataMng = {
 			}
 			else {
 				//console.log(rows[0].title+ " | " + rows[0].ip);
-				ftp_account = rows;
+				this.push(rows);
+				//ftp_account = rows;
 			}
-		});
-		//console.log("1 . " + ftp_account[0].title + " | " + ftp_account[0].ip);
+		}, ftp_account);
+		console.log("1 . " + ftp_account[0].title + " | " + ftp_account[0].ip);
 		console.log("2 . " + ftp_account.title + " | " + ftp_account.ip);
 	}
 
