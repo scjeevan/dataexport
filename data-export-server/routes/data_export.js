83215d915e027b69fb5b74d2694a80b722f60fca
Jeevan Pussella <scjeevan@gmail.com>
2016-07-03 21:01:50 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -6,37 +6,43 @@ var mysql_client = mysql.createConnection({
     password: process.env.DATAEXPORT_MYSQL_PASSWORD,
     database: process.env.DATAEXPORT_MYSQL_DBNAME
 });
-var ftp_account = [];
+
 function setValue(value) {
 	ftp_account = value;
 	console.log(ftp_account);
 	console.log("1 . " + ftp_account[0].title + " | " + ftp_account[0].ip);
-	//console.log("2 . " + ftp_account.title + " | " + ftp_account.ip);
+}
+
+function exportData(req, res) {
+	var query = "";
+	var params = [];
+	var ftp_account = [];
+	var ftp_account_id = 1;
+	if (typeof req.body.ftp_account_id != 'undefined'){
+		ftp_account_id = parseInt(req.body.ftp_account_id);
+	}
+	query = "SELECT `title`, `username`, `password`, `ip`, `port`, `protocol` FROM `ftp_accounts` WHERE `ftp_account_id`=?";
+	params = [parseInt(req.body.ftp_account_id)];
+	var formatedQuery = mysql.format(query, params);
+	mysql_client.query(formatedQuery, function (err, rows) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			ftp_account = rows;
+		}
+	});
+	
 }
 
 var exportDataMng = {
 	
 	exportData: function (req, res) {
-		var query = "";
-		var params = [];
-		
-		var ftp_account_id = 1;
-		if (typeof req.body.ftp_account_id != 'undefined'){
-			ftp_account_id = parseInt(req.body.ftp_account_id);
-		}
-		query = "SELECT `title`, `username`, `password`, `ip`, `port`, `protocol` FROM `ftp_accounts` WHERE `ftp_account_id`=?";
-		params = [parseInt(req.body.ftp_account_id)];
-		var formatedQuery = mysql.format(query, params);
-		mysql_client.query(formatedQuery, function (err, rows) {
-			if (err) {
-				console.log(err);
-			}
-			else {
-				//console.log(rows[0].title+ " | " + rows[0].ip);
-				setValue(rows)
-			}
-		});
-		
+		exportData(function (req, result) {
+            res.json({
+                values: result
+            });
+        });
 	}
 	
 	
