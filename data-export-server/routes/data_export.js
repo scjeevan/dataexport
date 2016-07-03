a7074a6925079944e1814749abd2c2052677781e
Jeevan Pussella <scjeevan@gmail.com>
2016-07-03 21:08:43 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -7,39 +7,36 @@ var mysql_client = mysql.createConnection({
     database: process.env.DATAEXPORT_MYSQL_DBNAME
 });
 
-function exportData(req, res) {
-	var query = "";
-	var params = [];
-	var ftp_account = [];
-	var ftp_account_id = 1;
-	if (typeof req.body.ftp_account_id != 'undefined'){
-		ftp_account_id = parseInt(req.body.ftp_account_id);
-	}
-	query = "SELECT `title`, `username`, `password`, `ip`, `port`, `protocol` FROM `ftp_accounts` WHERE `ftp_account_id`=?";
-	params = [parseInt(req.body.ftp_account_id)];
-	var formatedQuery = mysql.format(query, params);
-	mysql_client.query(formatedQuery, function (err, rows) {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			ftp_account = rows;
-		}
-	});
-	console.log(ftp_account);
-}
-
 var exportDataMng = {
 	
 	exportData: function (req, res) {
-		exportData(function (req, result) {
-            res.json({
-                values: result
-            });
-        });
+		var query = "";
+		var params = [];
+		var ftp_account = [];
+		var ftp_account_id = 1;
+		if (typeof req.body.ftp_account_id != 'undefined'){
+			ftp_account_id = parseInt(req.body.ftp_account_id);
+		}
+		query = "SELECT `title`, `username`, `password`, `ip`, `port`, `protocol` FROM `ftp_accounts` WHERE `ftp_account_id`=?";
+		params = [ftp_account_id];
+		var formatedQuery = mysql.format(query, params);
+		mysql_client.query(formatedQuery, function (err, rows) {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				//console.log(rows[0].title+ " | " + rows[0].ip);
+				ftp_account =  rows[0];
+			}
+		});
+		//console.log("1 . " + ftp_account[0].title + " | " + ftp_account[0].ip);
+		console.log("2 . " + ftp_account.title + " | " + ftp_account.ip);
 	}
 	
-	
+	function setValue(value) {
+		someVar = value;
+		console.log(someVar);
+	}
 
 };
 
