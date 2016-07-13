da32fcd0fcf5066a27f95d94c707faadf357cf4f
Jeevan Pussella <scjeevan@gmail.com>
2016-07-13 22:23:25 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/gqtest.js
+++ data-export-server/routes/gqtest.js
@@ -9,13 +9,11 @@ var bigquery = gcloud.bigquery({
 });
  
 function testDB(callback) {
-	/*
-	bigquery.job.query('diggit-1266', 'select count(*) from diggit-1266:diggit_hist.Diggit_IP', function(e,r,d){
+	bigquery.query('diggit-1266', 'select count(*) from diggit-1266:diggit_hist.Diggit_IP', function(e,r,d){
 		if(e) console.log(e);
 		console.log(JSON.stringify(d));
 		callback(d);
 	});
-	*/
 }
 
 //connection.end();
