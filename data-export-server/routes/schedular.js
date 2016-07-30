7d5cb3ad746899d066c2388f2aa2ed751a000fa9
Jeevan Pussella <scjeevan@gmail.com>
2016-07-31 01:25:13 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/schedular.js
+++ data-export-server/routes/schedular.js
@@ -3,7 +3,15 @@ var mysql = require("mysql");
 var fs = require("file-system");
 
 var date = new Date();
+var month = date.getMonth() + 1;
+
+var day  = date.getDate();
+day = (day < 10 ? "0" : "") + day;
+
 
 var j = schedule.scheduleJob('0 * * * * *', function(){
-	console.log('Data Export Job Runs at ' + date);
+	console.log('Data Export Job Runnig at ' + date);
+	console.log('month : ' + month);
+	console.log('day : ' + day);
+	
 });
