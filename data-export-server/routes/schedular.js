9f8462fec9d3ac8b2021ccc520a0671c5a09f840
Jeevan Pussella <scjeevan@gmail.com>
2016-07-31 01:36:28 +0530

    commit message

--- data-export-server/routes/schedular.js
+++ data-export-server/routes/schedular.js
@@ -3,15 +3,15 @@ var mysql = require("mysql");
 var fs = require("file-system");
 
 var date = new Date();
+
 var month = date.getMonth() + 1;
-
 var day  = date.getDate();
-day = (day < 10 ? "0" : "") + day;
-
+var weekDay = date.getDay();
 
 var j = schedule.scheduleJob('0 * * * * *', function(){
 	console.log('Data Export Job Runnig at ' + date);
 	console.log('month : ' + month);
 	console.log('day : ' + day);
+	console.log('weekDay : ' + weekDay);
 	
 });
