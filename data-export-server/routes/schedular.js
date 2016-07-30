61a9a9575f658e6ca1eaf402f30e0af05cd68e35
Jeevan Pussella <scjeevan@gmail.com>
2016-07-31 01:20:44 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/schedular.js
+++ data-export-server/routes/schedular.js
@@ -2,6 +2,8 @@ var schedule = require('node-schedule');
 var mysql = require("mysql");
 var fs = require("file-system");
 
+var date = new Date();
+
 var j = schedule.scheduleJob('0 * * * * *', function(){
-	console.log('The answer to life, the universe, and everything!');
+	console.log('Data Export Job Runs at ' + date);
 });
