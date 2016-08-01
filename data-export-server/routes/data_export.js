8c826ab776be23966323ec92887800adf1e0f690
Jeevan Pussella <scjeevan@gmail.com>
2016-08-01 21:13:57 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -128,7 +128,7 @@ var exportDataMng = {
 		var end = req.body.endDate.replace(/T/, ' ').replace(/\..+/, '');
 		var file_name = req.body.table + "-" + Math.floor(new Date() / 1000) + ".csv";
 		if(req.body.table == 'ip'){
-			_query += " FROM DevDiggit_Hist.Diggit_IP WHERE Date BETWEEN '"+start+"' AND '"+end+"' LIMIT 100000";
+			_query += " FROM DevDiggit_Hist.Diggit_IP WHERE Date BETWEEN '"+start+"' AND '"+end+"' "; //LIMIT 100000
 			console.log("[QUERY]:"+_query);
 			bigquery.startQuery(_query, function(err, job) {
 				if (!err) {
