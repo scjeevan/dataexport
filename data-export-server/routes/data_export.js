b07fd25ac0f9165661f5e7958e8894515b9c51a2
Jeevan Pussella <scjeevan@gmail.com>
2016-10-06 01:52:13 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -678,8 +678,8 @@ function processToExport(row, startDate, endDate, callback) {
 		password: row.password
 	};
 	if(tableName == 'Diggit_IP'){
-		query.replace('<start>', startDate);
-		query.replace('<end>', endDate);
+		//.replace('<start>', startDate);
+		//query.replace('<end>', endDate);
 		console.log(query);
 		//exportDataUsingScript(query, connProps, fileName+"_IP");
 		callback("SUCCESS");
