a81d1c0b8fa82a1cd79a7b9dd7e4b32414af513d
Jeevan Pussella <scjeevan@gmail.com>
2016-10-10 20:50:52 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-client/public/js/app.js
+++ data-export-client/public/js/app.js
@@ -747,6 +747,58 @@ mvpApp.directive('datepickerPopup', function (dateFilter,$parse){
 	}
 });
 
+mvpApp.directive('leaf', function (tree, ivhTreeviewMgr,$window,apiService) {
+	return { 
+		restrict: 'AE', 
+		link: function (scope, element, attrs) {
+			element.on('click', function () {
+				var root = scope.trvw.root();
+				if (scope.node.type == 'S') { 
+					return;
+				}
+				if (scope.node.children!==null & scope.node.children.length > 0) {
+					if (scope.node.children[0].type != 'DEL') {
+						return;//Loaded Parent, not need to load again
+					}
+				}
+				var config = {
+					params: {
+						id: scope.node.id
+					}
+				};
+				var url = "";
+				var fakechild = true;
+				switch (scope.node.type) {
+					case 'C1': 
+						url = 'api/your URL/';
+						fakechild = false;
+						break;
+					case 'S':  //Is the last node. need to stop
+						break;
+				}
+				apiService.get(url, config, loadCompleted, loadFailed);
+				function loadCompleted(result) {
+					if (result.status == 200) {
+						if (result.data != "") {
+							tree.genNode(result.data, scope.node, fakechild); 
+						} else {
+							if (scope.node.children[0].type == 'DEL') {
+								delete scope.node.children 
+							}  
+						}
+					} else {
+						$window.alert('Tree leaf loaded failed - ' + scope.node.type + ' ' + result.error);
+					}
+				}
+				function loadFailed(result) {
+					ivhTreeviewMgr.collapse(root, scope.node); 
+					$window.alert('Tree leaf loaded failed - ' + scope.node.type + ' ' + result.error);
+				}
+			});
+		}
+	};
+});
+
 mvpApp.controller('NavBarCtrl', ['$scope', '$location', function($scope, $location) {
 
 }]);
--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -29,22 +29,17 @@ var db_config = {
 };
 
 function handleDisconnect() {
-	mysql_client = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-	mysql_client.connect(function(err) {              // The server is either down
-		if(err) {                                     // or restarting (takes a while sometimes).
+	mysql_client = mysql.createConnection(db_config);
+	mysql_client.connect(function(err) {
+		if(err) {
 			console.log('error when connecting to db:', err);
-			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-		}                                     // to avoid a hot loop, and to allow our node script to
-	});                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+			setTimeout(handleDisconnect, 2000);
+		}
+	});
+
 	mysql_client.on('error', function(err) {
 		console.log('db error', err);
-		if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-			handleDisconnect();                         // lost due to either server restart, or a
-		} else {                                      // connnection idle timeout (the wait_timeout
-			throw err;                                  // server variable configures this)
-		}
+		handleDisconnect();
 	});
 }
 
