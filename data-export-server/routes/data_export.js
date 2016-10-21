7ee96753527075392b3427af3114d764b3cf848a
Jeevan Pussella <scjeevan@gmail.com>
2016-10-21 13:39:35 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/data_export.js
+++ data-export-server/routes/data_export.js
@@ -281,16 +281,8 @@ var exportDataMng = {
     },
 	
 	continents : function (req, res) {
-		var query = 'SELECT Continent AS name FROM DevDiggit_Hist.unique_ip_count_for_location GROUP BY name ORDER BY name';
-		var contArray = [];
-		executeGoogleBigQueryAllRows(query,function(rows){
-            rows.forEach(function(movie){
-                if(movie != null && movie.name != '') 
-					contArray.push({name: movie.name});
-            });
-            res.json(contArray);
-        });
-		//responseAll(query, res);
+		var query = 'SELECT Continent AS name FROM DevDiggit_Hist.ip_aggregation GROUP BY name ORDER BY name';
+		responseAll(query, res);
 	},
 	countries : function (req, res) {
 		var query = "SELECT Country AS name FROM DevDiggit_Hist.unique_ip_count_for_location WHERE Continent = '" + escape(req.params.continent) + "' GROUP BY name ORDER BY name";
