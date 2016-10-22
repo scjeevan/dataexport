829f02fbe44a40ecaab1593104a1ce7190a73703
Jeevan Pussella <scjeevan@gmail.com>
2016-10-22 23:32:42 +0530

    Signed-off-by: Jeevan Pussella <scjeevan@gmail.com>

--- data-export-server/routes/ftp_accounts.js
+++ data-export-server/routes/ftp_accounts.js
@@ -44,8 +44,11 @@ var ftpAccountsData = {
 		var username = req.body.username;
 		var password = req.body.password;
 		if (typeof req.body.ftp_account_id == 'undefined'){
-			var command = 'sudo /opt/script_sftp/addsftpuser.sh ' + username + ' ' + password;
+			var command = '/opt/script_sftp/addsftpuser.sh ' + username + ' ' + password;
 			var session = nodemiral.session(HOST, {username: 'jeevan', pem: fs.readFileSync(KEY_PATH).toString('utf8').trim()});
+			session.execute('sudo su', function(err, code, logs) {
+				console.log(logs.stdout);
+			});
 			session.execute(command, function(err, code, logs) {
 				console.log(logs.stdout);
 				query = "INSERT INTO `ftp_accounts` (`title`,`username`,`password`,`ip`,`port`,`location`,`protocol`) VALUES (?,?,?,?,?,?,?)";
